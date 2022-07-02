e228efe877ed55d252f7c50462fd52fbc9eca94e
dyakonovr <dyakonov.rr@bk.ru>
2022-07-02 21:47:08 +0400

    сверстал header, hero и tours

---
+++ src/js/components/swiper.js
@@ -0,0 +1,16 @@
+import Swiper, { Navigation, Scrollbar } from 'swiper';
+
+Swiper.use([Navigation, Scrollbar]);
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+});
